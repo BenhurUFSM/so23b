1828d1286a7de7f9d173501fc65a6e24fa52c3cf
Benhur Stein <benhur@inf.ufsm.br>
2023-08-28 19:02:23 -0300

    definição das primeiras chamadas de sistema, SO_LE e SO_ESCR

--- Trabalhos/t1/so.h
+++ Trabalhos/t1/so.h
@@ -14,4 +14,14 @@ typedef struct so_t so_t;
 so_t *so_cria(cpu_t *cpu, mem_t *mem, console_t *console);
 void so_destroi(so_t *self);
 
+// Chamadas de sistema (a chamada está em A quando executa CHAMASIS)
+
+// lê um caractere do dispositivo de entrada
+// retorna em A: o caractere lido ou um código de erro negativo
+#define SO_LE          1
+
+// escreve o caractere em X no dispositivo de saída
+// retorna em A: 0 se OK ou um código de erro negativo
+#define SO_ESCR        2
+
 #endif // SO_H
